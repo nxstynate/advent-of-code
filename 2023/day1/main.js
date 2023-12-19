09c8bf0fabda595ccb528da5b66e78efd584b715
Nate Lewis <nathanael.paul.lewis@gmail.com>
2023-12-19 16:28:14 -0600

    updating my commits

--- 2023/day1/main.js
+++ 2023/day1/main.js
@@ -1,38 +1,74 @@
-// loop through and get every number out of every line
-// push those numbers to a new array for each line
-// in that new array remove the middle number if there is more than two numbers
-// once the numbers have been cleaned up combine the two remaining numbers
-// fromt their move the numbers to the next array which will be the final array the number will be stored.
-// then run a sum up all the numbers
-
-// left off tyring to figure out how to check per line everything is laid out for the function.
+// import { calibrationDocument } from "./calibrationDoc.js";
 
 const calibrationDocument = `
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
 `;
 
-function keepFirstAndLastNumbers(arr) {
-  return arr.slice(0, 1).concat(arr.slice(-1));
-}
-
-function splitIntoArray(input) {
+function calibrationSplitIntoLines(input) {
   const inputIsSplitByLine = input.split("\n");
   const filteredArray = inputIsSplitByLine.filter(item => item !== "");
   return filteredArray;
 }
 
-function getStartAndEndNumber(input) {
+// the funtion can return some of the letters as numbers but if there is one than more number that is spelled out then it doesn't not work.
+// also figure out how to take the numbers and replace the spelled numbers in the calibration document
+// at the moment this function is not implemented into the other functions yet.
+function calibrationLettersToNumbers(input) {
+  let isStoredNumber = [];
+
+  const isSplitCalibrationDocument = calibrationSplitIntoLines(input);
+
+  for (let i = 0; i < isSplitCalibrationDocument.length; i++) {
+    if (isSplitCalibrationDocument[i].includes("one")) {
+      isStoredNumber.push(1);
+    } else if (isSplitCalibrationDocument[i].includes("two")) {
+      isStoredNumber.push(2);
+    } else if (isSplitCalibrationDocument[i].includes("three")) {
+      isStoredNumber.push(3);
+    } else if (isSplitCalibrationDocument[i].includes("four")) {
+      isStoredNumber.push(4);
+    } else if (isSplitCalibrationDocument[i].includes("five")) {
+      isStoredNumber.push(5);
+    } else if (isSplitCalibrationDocument[i].includes("six")) {
+      isStoredNumber.push(6);
+    } else if (isSplitCalibrationDocument[i].includes("seven")) {
+      isStoredNumber.push(7);
+    } else if (isSplitCalibrationDocument[i].includes("eight")) {
+      isStoredNumber.push(8);
+    } else if (isSplitCalibrationDocument[i].includes("nine")) {
+      isStoredNumber.push(9);
+    }
+  }
+  return isStoredNumber;
+}
+
+function calibrationValue(arr) {
+  const firstElement = arr.slice(0, 1);
+  const lastElement = arr.slice(-1);
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr.length === 1) {
+  //     return firstElement;
+  //   }
+  //   return firstElement.concat(lastElement);
+  // }
+  return firstElement.concat(lastElement);
+}
+
+function calibrationValueSum(input) {
   let isNumber = [];
   let isFinalNumber = [];
 
-  const isArray = splitIntoArray(input);
+  const isArray = calibrationSplitIntoLines(input);
 
   for (let i = 0; i < isArray.length; i++) {
     const hasRemovedLetters = isArray[i].replace(/[a-z]/gi, "");
-    const hasFirstAndLastNumbers = keepFirstAndLastNumbers(hasRemovedLetters);
+    const hasFirstAndLastNumbers = calibrationValue(hasRemovedLetters);
     const convertToInteger = parseInt(hasFirstAndLastNumbers);
     isNumber.push(convertToInteger);
   }
@@ -45,5 +81,5 @@ function getStartAndEndNumber(input) {
   return sumIsFinalNumbers;
 }
 
-const init = getStartAndEndNumber(calibrationDocument);
+const init = calibrationValueSum(calibrationDocument);
 console.log(init);
