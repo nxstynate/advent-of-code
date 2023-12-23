c748091c316dc7c42320124b388081b7f3ae6663
Nate Lewis <nathanael.paul.lewis@gmail.com>
2023-12-22 20:00:08 -0600

    removed old js files and replaced with ts files.

--- 2023/day1/calibrationSplitIntoLines.js
+++
@@ -1,9 +0,0 @@
-import { calibrationDocument } from "./tempCalibrationDoc.js";
-
-export function calibrationSplitIntoLines(input) {
-  const inputIsSplitByLine = input.split("\n");
-  const filteredArray = inputIsSplitByLine.filter(item => item !== "");
-  return filteredArray;
-}
-
-console.log(calibrationDocument);
